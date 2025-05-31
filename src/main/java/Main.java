37a210d0682fb25907a6ac8040eb53b0ce747b8c
ksuhavr <petrovich_kseniya@mail.ru>
2025-05-31 14:36:18 +0300

    сделала задание 2.5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,50 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String[][] correctArray = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"},
+        };
+        String[][] wrongSizeArray = {
+                {"1", "2", "3"},
+                {"5", "6", "7", "8"},
+                {"9", "10"},
+        };
+        String[][] wrongDataArray = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "X", "11", "12"},
+                {"13", "14", "15", "16"},
+        };
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //Обработка корректного массива
+        try {
+            System.out.println("Сумма корректного массива: " + Task.ArrayProcessor.processArray(correctArray));
+        } catch (Task.MyArraySizeException | Task.MyArrayDataException e) {
+            System.err.println("Ошибка: " + e.getMessage());
         }
+
+        //Обработка массива с неправильным размером
+        try {
+            System.out.println("Сумма массива с неправильным размером: " + Task.ArrayProcessor.processArray(wrongSizeArray));
+        } catch (Task.MyArraySizeException e) {
+            System.err.println("Ошибка размера массива: " + e.getMessage());
+        } catch (Task.MyArrayDataException e) {
+            System.err.println("Ошибка данных: " + e.getMessage());
+        }
+
+        //Обработка массива с некорректными данными
+        try {
+            System.out.println("Сумма массива с некорректными данными: " + Task.ArrayProcessor.processArray(wrongDataArray));
+        } catch (Task.MyArraySizeException | Task.MyArrayDataException e) {
+            System.err.println("Ошибка: " + e.getMessage());
+        }
+
+        //Демонстрация выхода за границы
+        System.out.println("\nДемонстрация ArrayIndexOutOfBoundsException: ");
+
+        Task.ArrayProcessor.generateIndexOutOfBounds(correctArray);
     }
 }
